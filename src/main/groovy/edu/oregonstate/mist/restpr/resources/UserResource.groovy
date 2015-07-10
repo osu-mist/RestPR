e2e7ebaa85494c8e6330ac4d75635d7354d2bdbe
George Crary <craryg@onid.oregonstate.edu>
2015-07-10 12:43:39 -0700

    Add user_login parameter to GET USER/ method and update to return Jax RS response codes to spec instead of raw JSON
    Add user_login parameter to GET USER/ method and update to return Jax RS response codes to spec instead of raw JSON

--- src/main/groovy/edu/oregonstate/mist/restpr/resources/UserResource.groovy
+++ src/main/groovy/edu/oregonstate/mist/restpr/resources/UserResource.groovy
@@ -50,10 +50,16 @@ class UserResource {
   }
 
   @GET
-  @Produces (MediaType.APPLICATION_JSON)
-  public List<User> getUsers(@NotNull @QueryParam("display_name") Optional<String> display_name){
+  @Produces(MediaType.APPLICATION_JSON)
+  //Entity type: List<User>
+  public Response getUsers(@NotNull @QueryParam("user_login") Optional<String> user_login ,
+                             @NotNull @QueryParam("display_name") Optional<String> display_name) {
     //System.out.println("DEBUGGING DISPLAY_NAME VARIABLE USER GET %"+ display_name.or("") +"%");
-    return userDAO.getPRUSERSmatch(display_name.or(""))
+    List<User> returnList = userDAO.getPRUSERSmatch(user_login.or("") , display_name.or(""))
+    returnList.each { debugPrintUser(it) }
+
+    def returnResponse = Response.ok(returnList).build()
+    return returnResponse
 
   }
 
