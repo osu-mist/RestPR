21f2e7bc02720a4e0ffb844e7ddff4fba2efc8d2
George Crary <craryg@onid.oregonstate.edu>
2015-07-13 16:24:31 -0700

    Add SeasonResource to edu.oregonstate.mist.restpr.resources package
    Add SeasonResource to edu.oregonstate.mist.restpr.resources package

---
+++ src/main/groovy/edu/oregonstate/mist/restpr/resources/SeasonResource.groovy
@@ -0,0 +1,40 @@
+package edu.oregonstate.mist.restpr.resources
+
+import com.google.common.base.Optional
+
+import edu.oregonstate.mist.restpr.api.ErrorPOJO
+import edu.oregonstate.mist.restpr.api.Season
+import edu.oregonstate.mist.restpr.api.User
+import edu.oregonstate.mist.restpr.db.SeasonDAO;
+import edu.oregonstate.mist.restpr.db.UserDAO
+
+import javax.validation.constraints.NotNull
+
+import javax.ws.rs.Consumes
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET
+import javax.ws.rs.POST
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by georgecrary on 7/13/15.
+ */
+
+@Path("/Season")
+@Produces(MediaType.APPLICATION_JSON)
+class SeasonResource {
+  private final SeasonDAO seasonDAO;
+
+  SeasonResource(SeasonDAO seasonDAO) {
+    this.seasonDAO = seasonDAO
+  }
+
+
+
+}
