ef32f985ddbbb67acb7ee448740d007ef8f91aba
George Crary <craryg@onid.oregonstate.edu>
2015-08-06 14:10:07 -0700

    Remove explicit getters and setters
    Remove explicit getters and setters. https://github.com/osu-mist/RestPR/commit/5ca312747aff63902d2663ab0d43af6ab06b9c80#commitcomment-12187825

--- src/main/groovy/edu/oregonstate/mist/restpr/api/User.groovy
+++ src/main/groovy/edu/oregonstate/mist/restpr/api/User.groovy
@@ -11,9 +11,9 @@ import javax.xml.bind.annotation.XmlRootElement
 
 @XmlRootElement(name = "user")
 class User{
-  private Integer user_id
-  private String user_login
-  private String display_name
+  Integer user_id
+  String user_login
+  String display_name
 
   public User(){
     // Jackson deserialization
@@ -23,37 +23,5 @@ class User{
     this.user_id = user_id
     this.user_login = user_login
     this.display_name = display_name
-
   }
-
-  @JsonProperty
-  Integer getUserId(){
-    return user_id
-  }
-  @JsonProperty
-  void setUserId(UserId){
-    this.user_id = UserId
-  }
-
-
-  @JsonProperty
-  public String getUserLogin(){
-    return user_login
-  }
-
-  @JsonProperty
-  public String getDisplayName(){
-    return display_name
-  }
-
-  @JsonProperty
-  void setUserLogin(UserLogin){
-    this.user_login = UserLogin
-  }
-
-  @JsonProperty
-  void setDisplayName(DisplayName){
-    this.display_name = DisplayName
-  }
-
 }
