b7e8947317b52feaade97599d7e51cfdefd443e7
George Crary <craryg@onid.oregonstate.edu>
2015-08-11 15:59:38 -0700

    Add javadoc comments to public methods in SeasonDAO and SeasonResource
    Add javadoc comments to public methods in SeasonDAO and SeasonResource

--- src/main/groovy/edu/oregonstate/mist/restpr/db/SeasonDAO.groovy
+++ src/main/groovy/edu/oregonstate/mist/restpr/db/SeasonDAO.groovy
@@ -13,6 +13,16 @@ import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper
 @RegisterMapper(SeasonMapper)
 public interface SeasonDAO extends Closeable  {
 
+  /**
+   * Selects seasons that match the double ended wildcard match clauses using community_name_match, cycle_format_match
+   * cycle_count_match, and year_match. In the event these parameters are empty strings it selects all seasons.
+   * @param community_name_match  Community name to be partially matched for
+   * @param cycle_format_match    Cycle Format to be partially matched for
+   * @param cycle_count_match     Cycle count to be partially matched for
+   * //TODO consider changing year to strict equality matching
+   * @param year_match            Year to be partially matched for
+   * @return all seasons that match the parameter partial like clauses
+   */
   @SqlQuery("""
             SELECT *
             FROM SEASON
@@ -28,12 +38,24 @@ public interface SeasonDAO extends Closeable  {
                               @Bind("cycle_count_match") String cycle_count_match,
                               @Bind("year_match") Integer year_match)
 
+  /**
+   * Selects all seasons from the season table
+   * @return all seasons from the table
+   */
   @SqlQuery("""
             SELECT *
             FROM SEASON
             """)
   List<Season> allSeasons()
 
+  /**
+   * Inserts a new season into the season table and uses the season sequence for the id
+   * @param community_name    Community name of the new season to be inserted
+   * @param cycle_format      Cycle format of the new season to be inserted
+   * @param cycle_count       Cycle count of the new season to be inserted
+   * @param elo_default_seed  Elo default seed of the new season to be inserted
+   * @param year              Year of the new season to be inserted
+   */
   @SqlUpdate("""INSERT INTO SEASON (SEASON_ID, COMMUNITY_NAME, CYCLE_FORMAT, CYCLE_COUNT, ELO_DEFAULT_SEED, SYEAR)
                 VALUES (SEASON_SEQ.NEXTVAL, :community_name, :cycle_format, :cycle_count, :elo_default_seed, :year)
              """)
@@ -41,6 +63,15 @@ public interface SeasonDAO extends Closeable  {
                   @Bind("cycle_count") String cycle_count, @Bind("elo_default_seed") Integer elo_default_seed,
                   @Bind("year") Integer year)
 
+  /**
+   * Updates a specified season row using the season_id parameter using the other parameters as the updated values
+   * @param season_id         Season id of the season record to be updated
+   * @param community_name    Community name of the season record to be updated
+   * @param cycle_format      Cycle format of the season record to be updated
+   * @param cycle_count       Cycle count of the season record to be updated
+   * @param elo_default_seed  Elo default seed of the season record to be updated
+   * @param year              Year of the season record to be updated
+   */
   @SqlUpdate( """
               UPDATE SEASON
               SET SEASON_ID = :season_id , COMMUNITY_NAME = :community_name , CYCLE_FORMAT = :cycle_format ,
@@ -51,6 +82,11 @@ public interface SeasonDAO extends Closeable  {
                @Bind("cycle_format") String cycle_format, @Bind("cycle_count") String cycle_count,
                @Bind("elo_default_seed") Integer elo_default_seed, @Bind("year") Integer year)
 
+  /**
+   * Selects and returns a specific season using the season_id
+   * @param season_id Season_id of the season record to be selected and returned
+   * @return a specific season using the season_id given
+   */
   @SqlQuery("""
             SELECT *
             FROM SEASON
@@ -58,6 +94,15 @@ public interface SeasonDAO extends Closeable  {
             """)
   Season getSeasonById(@Bind("season_id") Integer season_id)
 
+  /**
+   * Inserts a season record to a specified row using the season id
+   * @param season_id         Season_id of the row to be inserted to
+   * @param community_name    Community name of the row to be inserted with
+   * @param cycle_format      Cycle format of the row to be inserted with
+   * @param cycle_count       Cycle count of the row to be inserted with
+   * @param elo_default_seed  Elo default seed of the row to be inserted with
+   * @param year              Year of the row to be inserted with
+   */
   @SqlUpdate("""INSERT INTO SEASON (SEASON_ID, COMMUNITY_NAME, CYCLE_FORMAT, CYCLE_COUNT, ELO_DEFAULT_SEED, SYEAR)
                 VALUES (:season_id, :community_name, :cycle_format, :cycle_count, :elo_default_seed, :year)
              """)
@@ -65,12 +110,20 @@ public interface SeasonDAO extends Closeable  {
                         @Bind("cycle_format") String cycle_format, @Bind("cycle_count") String cycle_count,
                         @Bind("elo_default_seed") Integer elo_default_seed, @Bind("year") Integer year)
 
+  /**
+   * Deletes a specified season row from the table
+   * @param season_id season_id of the season to be deleted
+   */
   @SqlUpdate( """
               DELETE FROM SEASON
               WHERE SEASON_ID = :season_id
               """)
   void deleteSeasonById(@Bind("season_id") Integer season_id)
-
+  /**
+   * Returns the season_id of the latest season created using the season sequence
+   * //FIXME possible race condition
+   * @return the season_id of the latest season created using the season sequence
+   */
   @SqlQuery("""
             SELECT SEASON_SEQ.CURRVAL FROM DUAL
             """)
--- src/main/groovy/edu/oregonstate/mist/restpr/resources/SeasonResource.groovy
+++ src/main/groovy/edu/oregonstate/mist/restpr/resources/SeasonResource.groovy
@@ -41,6 +41,15 @@ class SeasonResource {
             +" "+season.getCycle_count()+" "+season.getElo_default_seed()+" "+season.getYear())
   }
 
+  /**
+   * Returns all the seasons that partially match the parameters using sql like %PARAM% clauses
+   * @param community_name  Name of the community to partially match by
+   * //TODO Consider changing format and count from partial matching to strict matching
+   * @param cycle_format    Format of the season to partially match by
+   * @param cycle_count     Cycle count of the season to partially match by
+   * @param year            Year of the season to partially match by
+   * @return                all the seasons that partially match the parameters
+   */
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   //Entity Type: List<Season>
@@ -58,6 +67,11 @@ class SeasonResource {
     Response.ok(returnList).build()
   }
 
+  /**
+   *  Posts new season into season table as long as its community name is unique
+   * @param newSeason The new season to be posted to the table
+   * @return Response code with uri of the newly created season
+   */
   @POST
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
@@ -96,6 +110,10 @@ class SeasonResource {
     returnResponse
   }
 
+  /**
+   * Returns all the seasons in the table
+   * @return all the seasons in the table within response body
+   */
   @Path("/all")
   @GET
   @Produces(MediaType.APPLICATION_JSON)
@@ -109,6 +127,11 @@ class SeasonResource {
     returnResponse
   }
 
+  /**
+   * Returns the specified season using the season_id
+   * @param season_id The season id of the season to be returned
+   * @return the season specified using the season_id
+   */
   @Path("/{season_id}")
   @GET
   @Produces(MediaType.APPLICATION_JSON)
@@ -129,6 +152,12 @@ class SeasonResource {
     returnResponse
   }
 
+  /**
+   * Updates a specified season with a new season representation
+   * @param season_id The season id of the specified season
+   * @param newSeason The representation of the new season
+   * @return An okay response on success
+   */
   @Path("/{season_id}")
   @PUT
   @Consumes(MediaType.APPLICATION_JSON)
