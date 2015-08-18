87546fb66174847c406b7c10e9733bc8db1d14c1
George Crary <craryg@onid.oregonstate.edu>
2015-08-17 17:01:18 -0700

    Add explicit equals methods to api models to fix unit test assertion behavior
    Add explicit equals methods to api models to fix unit test assertion behavior

--- src/main/groovy/edu/oregonstate/mist/restpr/api/ErrorPOJO.groovy
+++ src/main/groovy/edu/oregonstate/mist/restpr/api/ErrorPOJO.groovy
@@ -7,5 +7,16 @@ class ErrorPOJO {
   String errorMessage
   Integer errorCode
 
+  boolean equals(o) {
+    if (this.is(o)) return true
+    if (getClass() != o.class) return false
+
+    ErrorPOJO errorPOJO = (ErrorPOJO) o
+
+    if (errorCode != errorPOJO.errorCode) return false
+    if (errorMessage != errorPOJO.errorMessage) return false
+
+    return true
+  }
 
 }
--- src/main/groovy/edu/oregonstate/mist/restpr/api/User.groovy
+++ src/main/groovy/edu/oregonstate/mist/restpr/api/User.groovy
@@ -8,4 +8,17 @@ class User{
   Integer user_id
   String user_login
   String display_name
+
+  boolean equals(o) {
+    if (this.is(o)) return true
+    if (getClass() != o.class) return false
+
+    User user = (User) o
+
+    if (display_name != user.display_name) return false
+    if (user_id != user.user_id) return false
+    if (user_login != user.user_login) return false
+
+    return true
+  }
 }
